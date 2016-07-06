c18a13ec33b7b6fef49f61f9936cef6f12d9237d
henrym0503 <henrymorales050398@gmail.com>
2016-07-06 00:37:23 -0400

    going back to before

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongoose =  require('mongoose')
 var mongoDB = mongoose.connection
 
-mongoose.connect('mongodb://henrymorales.com/sexy')
+mongoose.connect('mongodb://localhost/sexy')
 
 mongoDB.on('error', console.error.bind(console, 'error'))
 
